262b2fb0775d787d5d74e4dac13fc561b4e3cd7e
andraz <edofic@gmail.com>
2012-02-07 16:16:21 +0100

    initial commit empty android project

---
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/IchibanActivity.java
@@ -0,0 +1,13 @@
+package com.abstracttech.ichiban;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IchibanActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
