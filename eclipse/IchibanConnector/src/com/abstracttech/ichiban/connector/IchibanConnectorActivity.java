dacc57896eee1f5f298e5d63a0f92679fe904a30
edofic <edofic@gmail.com>
2012-02-08 10:59:26 +0100

    projekt za BT strežnik in projekt za BT klienta za testiranje na telefonu(froyo)

---
+++ eclipse/IchibanConnector/src/com/abstracttech/ichiban/connector/IchibanConnectorActivity.java
@@ -0,0 +1,13 @@
+package com.abstracttech.ichiban.connector;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IchibanConnectorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
