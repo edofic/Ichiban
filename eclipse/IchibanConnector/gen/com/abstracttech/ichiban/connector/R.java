d22c8a9d70bc27f0c0c2934a22cbc7ebf619b873
andraz <edofic@gmail.com>
2012-02-08 12:06:45 +0100

    ichibanserver = derivat bluetooth chat from android sdk

--- eclipse/IchibanConnector/gen/com/abstracttech/ichiban/connector/R.java
+++ eclipse/IchibanConnector/gen/com/abstracttech/ichiban/connector/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.abstracttech.ichiban;
+package com.abstracttech.ichiban.connector;
 
 public final class R {
     public static final class attr {
