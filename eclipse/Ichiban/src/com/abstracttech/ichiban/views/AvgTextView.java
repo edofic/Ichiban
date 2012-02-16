33309e1cec408edcff3c25aed9131cd8818c1529
edofic <edofic@gmail.com>
2012-02-16 12:49:30 +0100

    statistic data display

---
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/views/AvgTextView.java
@@ -0,0 +1,36 @@
+package com.abstracttech.ichiban.views;
+
+import com.abstracttech.ichiban.data.Data;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class AvgTextView extends TextView {
+	private float value;
+
+	public AvgTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		Data.subscribe(this);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		if(isInEditMode()) //dummy data for editor
+			this.setText("53.567");
+		else
+		{
+			float d = Data.statistic.getAvg(); //data from datasource
+			if(d!=value)
+			{
+				value=d;
+				this.setText(String.format("%.2f", d));
+			}
+		}
+		
+		super.onDraw(canvas);
+	}
+
+}
