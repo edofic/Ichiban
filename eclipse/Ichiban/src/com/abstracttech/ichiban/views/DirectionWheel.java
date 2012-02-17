ebc4aab9ec58b0fc17e459318d3e9709cafdc9b5
aljosa_mrak <aljosa.mrak@gmail.com>
2012-02-17 14:23:24 +0100

    DirectionWheel update

--- eclipse/Ichiban/src/com/abstracttech/ichiban/views/DirectionWheel.java
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/views/DirectionWheel.java
@@ -7,17 +7,21 @@ import android.util.AttributeSet;
 import android.widget.ImageView;
 
 import com.abstracttech.ichiban.R;
+import com.abstracttech.ichiban.activities.IchibanActivity;
 import com.abstracttech.ichiban.data.Data;
 
 public class DirectionWheel extends ImageView {
 	
 	private Matrix matrix;									//rotation matrix
+	private float preData = 0;
+	private float rotation = 0;
+	
 	
 	public DirectionWheel(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		
 		this.matrix = new Matrix();
-		this.setImageResource(R.drawable.rpm_bg);	
+		this.setImageResource(R.drawable.volan);	
 		this.setScaleType(ScaleType.MATRIX);		//Uses matrix to change image 
 		// TODO Auto-generated constructor stub
 	}
@@ -28,9 +32,12 @@ public class DirectionWheel extends ImageView {
 		matrix.reset();		//reseting matrix
 		
 		if(isInEditMode()) //dummy data for editor
-			matrix.setRotate((float) (120 * 0.5f - 60), (this.getLeft() + this.getRight()) / 2  , (this.getTop()+ this.getBottom()) / 2);
+			matrix.setRotate((float) (120 * 0.5f - 80), this.getWidth() / 2  , this.getHeight() / 2);
 		else
-			matrix.setRotate((float) (120 * Data.getTurnRatioPercentage() - 60), (this.getLeft() + this.getRight()) / 2  , (this.getTop()+ this.getBottom()) / 2);
+		{				
+			rotation = ((160 * Data.getTurnRatioPercentage() - 80) - rotation) / 2;//(preRot -  120 * Data.getTurnRatioPercentage() - 60) / IchibanActivity._UPDATE_INTERVAL;
+			matrix.setRotate(rotation, this.getWidth() / 2  , this.getHeight() / 2);
+		}
 		
 		this.setImageMatrix(matrix);	//set new matrix
 		
