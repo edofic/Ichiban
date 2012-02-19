894b514c079b5099efe7809fe32952327966210d
andraz <edofic@gmail.com>
2012-02-19 22:31:16 +0100

    rpm statistics fix

--- eclipse/Ichiban/src/com/abstracttech/ichiban/data/RpmData.java
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/data/RpmData.java
@@ -4,14 +4,30 @@ public class RpmData extends StatisticData {
 	@Override
 	public void update() {
 		float d = Data.getRpm();
-		
+
 		if(d>cMax)
 			cMax=d;
-		if(d<cMin)
+		if(d<cMin && d>0)
 			cMin=d;
-		
+
 		//averaging
 		n++;
 		total+=d;
+	}
+
+	@Override
+	public float getMin(){
+		if(cMin==Integer.MAX_VALUE)
+			return 0;
+		else
+			return cMin;
+	}
+
+	@Override
+	public float getMax(){
+		if(cMax==Integer.MIN_VALUE)
+			return 0;
+		else
+			return cMax;
 	}
 }
