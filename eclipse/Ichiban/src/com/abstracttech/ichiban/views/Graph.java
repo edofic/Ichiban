01de8a0e9e7134d02fab46d2ac094e6ee2dab2fa
andraz <edofic@gmail.com>
2012-02-20 18:17:46 +0100

    merged graphs in sigle file let's do some OO programing shall we, not copy-pasting classes

--- eclipse/Ichiban/src/com/abstracttech/ichiban/views/Graph.java
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/views/Graph.java
@@ -1,6 +1,5 @@
 package com.abstracttech.ichiban.views;
 
-import java.lang.reflect.Method;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -8,71 +7,80 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.SystemClock;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.ImageView;
 
 import com.abstracttech.ichiban.activities.IchibanActivity;
+import com.abstracttech.ichiban.data.Data;
 
-public class Graph extends View {
+public abstract class Graph extends ImageView {
 	
-	private Method graphDataMethod;  	// dataSream method
 	private Queue data;					// Stack of data to be drawn
-	private long time = 0;				// local timer
-
-	/**
-	 * 
-	 * @param data = Method in Data. This determinate which data is used 
-	 */
+	protected float top,bottom;
+	
 	public Graph(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		
 		data = new LinkedList();
-		// TODO Auto-generated constructor stub
+		Data.subscribe(this);// 565 340
 	}
 	
-	/**
-	 * 
-	 * @param dataSource = Method in Data. This determinate which data is used 
-	 */
-	public void ChangeDataSorce(Method dataSource)
-	{
-		graphDataMethod = dataSource;		// determination new dataSorce
-		data.clear();			// restart data
-	}
-
+	protected abstract float GetData();
+	
 	@Override
 	protected void onDraw(Canvas canvas)
 	{
 		super.onDraw(canvas);
 		
-		if(isInEditMode()) //dummy data for editor
-		{}		
-		else if(!
-				data.isEmpty())
+		Paint p = new Paint();
+		p.setStrokeWidth(1);
+		
+		p.setColor(Color.BLACK);								//name
+		canvas.drawText("s(t)", this.getWidth() - 20, 20, p);
+		
+		p.setColor(Color.argb(255, 100, 100, 100));
+		
+		
+		for(int i = 0; i < 4; i++)		//side data: lines, percentage,... 
 		{
-			time += SystemClock.currentThreadTimeMillis();	// updating local timer
-			
-			//if (time > IchibanActivity._UPDATE_INTERVAL)	//data updated every update interval
-			{
-				time = 0;
-				data.add(graphDataMethod);												//updating data from predefined source
-				if (data.size() > 3000 / (float)IchibanActivity._UPDATE_INTERVAL)		//to store only 20 seconds of data
-					data.poll();
-			}
+			canvas.drawText("" + (i + 1) * 20 + "%", 2, this.getHeight() - (i + 1) * this.getHeight() / 4 - 2, p);
+			canvas.drawText("" + (i + 1) * 0.5f + "s", (i + 1) * this.getWidth() / 4, this.getHeight() - 6, p);
+			canvas.drawLine(0, (i + 1) * this.getHeight() / 4, this.getWidth(), (i + 1) * this.getHeight() / 4, p);
+			canvas.drawLine((i + 1) * this.getWidth() / 4, 0, (i + 1) * this.getWidth() / 4, this.getHeight(), p);
+		}
+		
+		if(!data.isEmpty() || GetData() != 0)
+		{
+			data.add(GetData());												//updating data from predefined source
+			if (data.size() > 3000 / (float)IchibanActivity._UPDATE_INTERVAL)		//to store only 20 seconds of data
+				data.poll();
+		}
+		
+		if(!data.isEmpty())
+		{					
 							
-			float[] points = new float[data.size() * 4];	//array of points of graph
+			float[] points = new float[data.size() * 4];			//array of points of graph
+			float[] pointsShadow = new float[data.size() * 4];		//shadow array
+			
+			
 			for (int i = 0; i < data.size() - 1; i++)		//Set points to draw lines
 			{
-				points[i * 4] =/* this.getLeft() */+ this.getWidth() * i / (float)data.size();													//x0
-				points[i * 4 + 1] = (float) (/*this.getTop()*/ + this.getHeight() * (1 - Double.parseDouble(data.toArray()[i].toString())));	//y0
-				points[i * 4 + 2] = /*this.getLeft() */+ this.getWidth() * (i + 1) / (float)data.size();										//x1
-				points[i * 4 + 3] = (float) (/*this.getTop() */+ this.getHeight() * (1 - Double.parseDouble(data.toArray()[i+1].toString())));	//y1
-			}				
+				points[i * 4] = this.getWidth() * i / (float)data.size();
+				points[i * 4 + 1] = this.getHeight() * (1 - Float.parseFloat(data.toArray()[i].toString()) / top) - 2;
+				points[i * 4 + 2] = this.getWidth() * (i + 1) / (float)data.size();
+				points[i * 4 + 3] = this.getHeight() * (1 - Float.parseFloat(data.toArray()[i + 1].toString()) / top) - 2;
+				
+				pointsShadow[i * 4] = this.getWidth() * i / (float)data.size() + 5;
+				pointsShadow[i * 4 + 1] = this.getHeight() * (1 - Float.parseFloat(data.toArray()[i].toString()) / top) + 5;
+				pointsShadow[i * 4 + 2] = this.getWidth() * (i + 1) / (float)data.size() + 5;
+				pointsShadow[i * 4 + 3] = this.getHeight() * (1 - Float.parseFloat(data.toArray()[i+1].toString()) / top) + 5;
+			}
 			
-			Paint p = new Paint();
+			canvas.drawLines(pointsShadow, p);
+			
 			p.setColor(Color.RED);							//setting paint (Color, stroke...) and drawing the lines
 			p.setStrokeWidth(4);
 			canvas.drawLines(points, p);
-			}	
+		}	
 	}
 }
