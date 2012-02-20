466b6f39d81bee7252941b4d5389b0392c24ca1c
marko <@marko>
2012-02-20 22:11:43 +0100

    rpm speed

---
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/views/speed/NowMain.java
@@ -0,0 +1,36 @@
+package com.abstracttech.ichiban.views.speed;
+
+import com.abstracttech.ichiban.data.Data;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class NowMain extends TextView {
+
+	private float value;
+
+	public NowMain(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		Data.subscribe(this);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		if(isInEditMode()) //dummy data for editor
+			this.setText("0");
+		else
+		{
+			float d = Data.speedData.getSpeed(); //data from datasource
+			if(d!=value)
+			{
+				value=d;
+				this.setText(String.format("%.0f", d));
+			}
+		}
+		
+		super.onDraw(canvas);
+	}
+}
