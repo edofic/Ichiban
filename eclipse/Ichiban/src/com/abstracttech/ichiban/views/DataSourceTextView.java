4aa3e75c078933906778aa5b4d36e28da8a0c36e
andraz <edofic@gmail.com>
2012-02-21 18:52:36 +0100

    datasource comments

--- eclipse/Ichiban/src/com/abstracttech/ichiban/views/DataSourceTextView.java
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/views/DataSourceTextView.java
@@ -1,12 +1,15 @@
 package com.abstracttech.ichiban.views;
 
-import com.abstracttech.ichiban.data.Data;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+import com.abstracttech.ichiban.data.Data;
+
+/**
+ * text view for displaying current data source
+ */
 public class DataSourceTextView extends TextView {
 
 	public DataSourceTextView(Context context, AttributeSet attrs) {
