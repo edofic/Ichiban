d841171142c8aedafd2d5b72d4f8e8f400b08d15
andraz <edofic@gmail.com>
2012-02-21 18:52:29 +0100

    wheel comments

--- eclipse/Ichiban/src/com/abstracttech/ichiban/views/DirectionWheel.java
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/views/DirectionWheel.java
@@ -2,13 +2,15 @@ package com.abstracttech.ichiban.views;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 import com.abstracttech.ichiban.R;
 import com.abstracttech.ichiban.data.Data;
 
+/**
+ * steering wheel that rotates with respect to turn ratio
+ */
 public class DirectionWheel extends ImageView {
 	public DirectionWheel(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -20,6 +22,7 @@ public class DirectionWheel extends ImageView {
 	@Override
 	protected void onDraw(Canvas canvas)
 	{
+		//draw rotated image set in xml
 		canvas.save();
 	     canvas.rotate(Data.getTurnRatioPercentage() * 50,this.getWidth()/2f,this.getHeight()/2f);
 	     super.onDraw(canvas);
