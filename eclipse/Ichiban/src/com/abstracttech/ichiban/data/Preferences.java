50215d3d00b27a0f16a26ee57724436690a48bb3
aljosa <aljosa.mrak@gmail.com>
2012-02-22 12:00:43 +0100

    preferences get

--- eclipse/Ichiban/src/com/abstracttech/ichiban/data/Preferences.java
+++ eclipse/Ichiban/src/com/abstracttech/ichiban/data/Preferences.java
@@ -1,9 +1,47 @@
 package com.abstracttech.ichiban.data;
 
+import android.content.SharedPreferences;
+
 /**
  * this holds global preferences
  */
 public class Preferences {
+
+	private static SharedPreferences pref;
+
+	/**
+	 * Initialize this from an activity class
+	 * @param Pref set this as a parameter PreferenceManager.getDefaultSharedPreferences(this)
+	 */
+	public static void Initalize(SharedPreferences Pref)
+	{
+		pref = Pref;
+	}
 	
-	public static boolean vibrate, sound, restartDat;	//All preferences data 
+	/**
+	 * 
+	 * @return vibrations settings. true if enabled false if disabled
+	 */
+	public static boolean getVibrate()
+	{
+		return pref.getBoolean("vibration", true);
+	}
+	
+	/**
+	 * 
+	 * @return sound settings. true if enabled false if disabled
+	 */
+	public static boolean getSound()
+	{
+		return pref.getBoolean("sound", true);
+	}
+	
+	/**
+	 * 
+	 * @return restart settings. true if data reset after button start is pressed, false if data is paused
+	 */
+	public static boolean getRestartData()
+	{
+		return !pref.getString("reset", "notRestart").equals("notRestart");
+	}
 }
